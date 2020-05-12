34a3a11cb0dea1359052d64516ebca8aaea2aa73
Omar <omar@fedora-ws.attlocal.net>
2020-05-12 19:16:59 -0400

    Added more comprehensive email regex

--- equity_lib.py
+++ equity_lib.py
@@ -29,20 +29,18 @@ def convert_date_string(df,col_name):
 
 def parse_email (s):
     """Replace moves removes all spaces: beginning middle and end"""
-    s = s.replace()
+    s = s.replace(' ','')
     """Parses a string as an email address, returning an (id, domain) pair."""
     pattern = '''
-    ^
-    (?P<user>[a-zA-Z][\w.\-+]*)
-    @
-    (?P<domain>[\w.\-]*[a-zA-Z])
-    $
+(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9]))\.){3}(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9])|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])
     '''
     matcher = re.compile(pattern, re.VERBOSE)
     matches = matcher.match(s)
     if matches:
-        return (matches.group('user'), matches.group('domain'))
-    raise ValueError("Bad email address")
+        return (matches.group(0))
+    else:
+        print(s)
+        raise ValueError("Bad email address")
 
 def parse_phone(s):
     """
